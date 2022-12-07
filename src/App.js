e45754f92a7b9f88ffdff992e405ca403e7032ac
DorcasOJ <ayanfedorcas2017@gmail.com>
2022-12-07 20:47:05 +0100

    react-admin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Topbar from './components/topBar/Topbar';
 import Home from './pages/home/Home';
+import UserList from './pages/userList/UserList';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
         <Sidebar />
         <Routes>
           <Route path='/' element={<Home />} />
+        </Routes>
+        <Routes>
+          <Route path='/users' element={<UserList />} />
         </Routes>
       </div>
     </div>
